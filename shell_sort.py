7b3e7fc4dfb5e70fd1239b27b8294d09cd5ccb7b
안검성(Geomseong Ahn) <84578465+gs97ahn@users.noreply.github.com>
2021-10-07 00:03:18 +0900

    Add files via upload

---
+++ shell_sort.py
@@ -0,0 +1,38 @@
+# 쉘 정렬(shell sort)
+def shellSort(a, n):
+    h = 1
+    while 3 * h + 1 < n:
+        h = 3 * h + 1
+    while h > 0:
+        for i in range(h+1, n+1):
+            v, j = a[i], i
+            while j > h and a[j-h] > v:
+                a[j] = a[j-h]
+                j -= h
+            a[j] = v
+        h = int(h/3)
+
+def checkSort(a, n):
+    isSorted = True
+    for i in range(1, n):
+        if (a[i] > a[i+1]):
+            isSorted = False
+        if (not isSorted):
+            break
+    if isSorted:
+        print("Sorting complete")
+    else:
+        print("Sorting error occurred")
+
+import random, time
+
+N = 10000
+a = []
+a.append(-1)
+for i in range(N):
+    a.append(random.randint(1, N))
+start_time = time.time()
+shellSort(a, N)
+end_time = time.time() - start_time
+print('Shell sort process time (N=%d) : %0.3f'%(N, end_time))
+checkSort(a, N)
