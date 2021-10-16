afffceb7922fbc7d63f20fc050341391091b85b8
안검성(Geomseong Ahn) <84578465+gs97ahn@users.noreply.github.com>
2021-10-16 14:11:16 +0900

    Add files via upload

---
+++ radix_sort.py
@@ -0,0 +1,59 @@
+# 기수 정렬(radix sort)
+def enqueue (queue, data):
+    queue.append(data)
+
+def dequeue(queue):
+    if len(queue) == 0:
+        print('Queue is empty')
+        return -1
+    else:
+        data = queue.pop(0)
+        return data
+
+def digit(d, k):
+    temp = 1
+    for i in range(1, k):
+        temp *= 10
+    d = int(d / temp)
+    d %= 10
+    return d
+
+def radixSort(a, n, m, queue):
+    for k in range(1, m + 1):
+        for i in range(1, n + 1):
+            kd = digit(a[i], k)
+            enqueue(queue[kd], a[i])
+        p = 0
+        for i in range(10):
+            while len(queue[i]) != 0:
+                p += 1
+                a[p] = dequeue(queue[i])
+
+def checkSort(a, n):
+    isSorted = True
+    for i in range(1, n):
+        if (a[i] > a[i + 1]):
+            isSorted = False
+        if (not isSorted):
+            break
+    if isSorted:
+        print("Sorting complete")
+    else:
+        print("Sorting error occurred")
+
+import random, time
+
+M = 5
+N = 100000
+a = []
+a.append(-1)
+for i in range(N):
+    a.append(random.randint(1, 99999))
+Q = []
+for i in range(10):
+    Q.append([])
+start_time = time.time()
+radixSort(a, N, M, Q)
+end_time = time.time() - start_time
+print('Radix sort process time (N=%d) : %0.3f' %(N, end_time))
+checkSort(a, N)
